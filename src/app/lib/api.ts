ed27212337f53d65250fef12f4e210b359f5db1a
ridho <adiridho94@gmail.com>
2025-05-24 02:46:44 +0700

    fix

--- src/app/lib/api.ts
+++ src/app/lib/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const api = axios.create({
     baseURL: process.env.NEXT_PUBLIC_API_URL, // e.g. “http://localhost:3000”
-    withCredentials: true,                    // otomatis kirim & terima cookie “jwt”
+    // withCredentials: true,                    // otomatis kirim & terima cookie “jwt”
 })
 
 export default api
