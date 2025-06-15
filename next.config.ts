7a509633f99464952096a7b919bdcb5e24bad307
ridho <adiridho94@gmail.com>
2025-06-16 01:05:07 +0700

    fix

--- next.config.ts
+++ next.config.ts
@@ -1,22 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    async headers() {
-        return [
-            {
-                source: '/:path*',             // for all routes
-                headers: [
-                    {
-                        key: 'X-Frame-Options',
-                        value: 'ALLOWALL',         // or 'SAMEORIGIN' if your PHP is on the same domain
-                    },
-                    {
-                        key: 'Content-Security-Policy',
-                        value: "frame-ancestors 'self' http://localhost:80;",
-                    },
-                ],
-            },
-        ];
-    },
     eslint: {
         // Warning: This allows production builds to successfully complete even if
         // your project has ESLint errors.
